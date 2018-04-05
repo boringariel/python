274d7d833591db967983867e4082fd1b7d89ae8d
boringariel <jwkang3929@naver.com>
2018-04-06 01:04:24 +0900

    2_change

--- 소스코드/vending_machine.py
+++ 소스코드/vending_machine.py
@@ -6,7 +6,7 @@ coin500 = int(input("500원 동전개수: "))
 coin100 = int(input("100원 동전개수: "))
 total = bill * 1000 + coin500 * 500 + coin100 * 100
 
-buyCoffee = int(input("%s 몇 잔을 구매할까요?: " %itemName))
+buyCoffee = int(input("%s 몇 잔을 구매할까요?: " % itemName))
 
 total = total - buyCoffee * itemPrice
 
@@ -15,4 +15,4 @@ total = total % 500
 
 nCoin100 = total // 100
 
-print("반환된 500원: %d 개\n반환된 100원: %d 개"%(nCoin500, nCoin100))
+print("반환된 500원: %d 개\n반환된 100원: %d 개" % (nCoin500, nCoin100))
