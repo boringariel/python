3e6815bd320dafdfc37c537e36ee7268988bf99e
boringariel <jwkang3929@naver.com>
2018-04-26 13:20:09 +0900

    3

---
+++ lecture/소스코드/compare.py
@@ -0,0 +1,9 @@
+num1 = input("첫 번째 숫자: ")
+num2 = input("두 번째 숫자: ")
+
+if num1 > num2:
+    print("첫 번째 숫자가 더 크네요")
+elif num1 < num2:
+    print("두 번째 숫자가 크네요")
+else:
+    print("두 수가 같네요")
--- lecture/소스코드/vending_machine_2.py
+++ lecture/소스코드/vending_machine_2.py
@@ -8,8 +8,13 @@ total = bill * 1000 + coin500 * 500 + coin100 * 100
 
 buyCoffee = int(input("%s 몇 잔을 구매할까요?: " % itemName))
 
-total = total - buyCoffee * itemPrice
-print("%s %d잔을 구매했습니다." %(itemName, buyCoffee))
+#+ 이 부분이 수정된 알고리즘에 해당하는 코드이다.
+if (total - buyCoffee * itemPrice) >= 0:
+  total = total - buyCoffee * itemPrice
+  print("%s %d잔을 구매했습니다." %(itemName, buyCoffee))
+else:
+  print("돈이 부족합니다. 남은 금액은 반환합니다.")
+#-
 
 nCoin500 = total // 500
 total = total % 500
